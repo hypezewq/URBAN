b017f1eaf2ef3db99bccaf3fa026b9b27baa96c3
hypezewq <quoiaaz@gmail.com>
2024-08-27 23:56:40 +0500

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -2,5 +2,13 @@ def print_params(a=1, b="строка", c=True):
     print(a, b, c)
 
 
-values_list_2 = [54.32, 'Строка' ]
-print_params(*values_list_2, 42)
+print_params(11)
+print_params(12, "asd")
+print_params(12, "asd", False)
+print_params()
+values_list = [1, "asdas", True]
+values_dict = {1: "aaa", "sss": 'False', True: '333'}
+print_params(*values_list)
+print_params(*values_dict)
+values_list2 = [1, "asdasd"]
+print_params(*values_list2, 42)
